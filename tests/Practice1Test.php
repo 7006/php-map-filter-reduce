8bbff76735d6f3c4f2b5706e3b5b831a59196e99
7006 <alexbox86@gmail.com>
2024-09-19 19:55:01 +0000

    skip unused vars when doing array destruction

--- tests/Practice1Test.php
+++ tests/Practice1Test.php
@@ -10,7 +10,7 @@ class Practice1Test extends TestCase {
     #[Test]
     public function it_paint_fruit_yellow() {
         $bucket = ['apple', 'potato'];
-        [[$apple, $appleColor], [$potato, $potatoColor]] = Practice1::solution($bucket);
+        [[, $appleColor], [, $potatoColor]] = Practice1::solution($bucket);
 
         $this->assertEquals($appleColor, 'yellow');
         $this->assertNotEquals($potatoColor, 'yellow');
@@ -19,7 +19,7 @@ class Practice1Test extends TestCase {
     #[Test]
     public function it_paint_vegetable_green() {
         $bucket = ['apple', 'potato'];
-        [[$apple, $appleColor], [$potato, $potatoColor]] = Practice1::solution($bucket);
+        [[, $appleColor], [, $potatoColor]] = Practice1::solution($bucket);
 
         $this->assertNotEquals($appleColor, 'green');
         $this->assertEquals($potatoColor, 'green');
