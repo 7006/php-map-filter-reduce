fd85092e08e93c5469f3b2d94ad433020cb08e52
7006 <alexbox86@gmail.com>
2024-09-19 19:15:50 +0000

    Add test example

---
+++ src/Practice1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Frob\Mfr;
+
+class Practice1 {
+    public static function solution(array $elements) {
+        return [['apple', 'yellow'], ['potato', 'green']];
+    }
+
+    public static function fp_solution() {
+
+    }
+}
---
+++ tests/Practice1Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Frob\Mfr\Tests;
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use Frob\Mfr\Practice1;
+
+class Practice1Test extends TestCase {
+    #[Test]
+    public function it_paint_fruit_yellow() {
+        $elements = ['apple', 'potato'];
+        [[$apple, $appleColor], [$potato, $potatoColor]] = Practice1::solution($elements);
+
+        $this->assertEquals($appleColor, 'yellow');
+        $this->assertNotEquals($potatoColor, 'yellow');
+    }
+}
