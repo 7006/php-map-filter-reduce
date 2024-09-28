cc697085fc89bfa5e2586e59656b40b6f73bdf7d
Oleg Romadin <osromadin@gmail.com>
2024-09-28 13:03:22 +0300

    Ex1 simple solution completed

--- src/Ex1.php
+++ src/Ex1.php
@@ -3,6 +3,7 @@
 namespace Telema;
 
 class Ex1 {
+    
     public static $ITEMS = [
         ['food' => 'apple'],
         ['food' => 'carrot'],
@@ -15,7 +16,45 @@ class Ex1 {
     ];
 
     public static function solution(array $items = []) {
+        $results = [];
+        foreach ($items as $item) {
+            $item['color'] = Ex1::foodColor($item['food']);
+            $results[] = $item;
+        }
+        return $results;
+    }
 
+    private static function foodColor($food) {
+
+        $color = 'blank';
+
+        switch($food) {
+            
+            case 'apple':
+                $color = 'yellow';
+                break;
+
+            case 'carrot':
+                $color = 'green';
+                break;
+
+            case 'beet':
+                $color = 'green';
+                break;
+
+            case 'lemon':
+                $color = 'yellow';
+                break;
+
+            case 'pear':
+                $color = 'yellow';
+                break;
+
+            case 'potato':
+                $color = 'green';
+                break;
+        }
+        return $color;
     }
 
     public static function fp_solution(array $items = []) {
