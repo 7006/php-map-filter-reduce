cfbf2d51ca778ae969016107c00fa1c2cd5a5513
Oleg Romadin <osromadin@gmail.com>
2024-09-28 14:10:51 +0300

    delete all changes

--- src/Ex1.php
+++ src/Ex1.php
@@ -17,32 +17,10 @@ class Ex1 {
     ];
 
     public static function solution(array $items = []) {
-        $results = [];
-        foreach ($items as $item) {
-            $item['color'] = Ex1::foodColor($item['food']);
-            $results[] = $item;
-        }
-        return $results;
-    }
-
-    private static function foodColor($food) {
-        $foodToColor = [
-           'apple' => 'yellow',
-           'carrot' => 'green',
-           'beet' => 'green',
-           'lemon' => 'yellow',
-           'pear' => 'yellow',
-           'potato' => 'green',
-        ];
-
-        if (isset($foodToColor[$food])) {
-            return $foodToColor[$food];
-        } else {
-            return 'blank';
-        } 
+        
     }
 
     public static function fp_solution(array $items = []) {
-
+        
     }
 }
