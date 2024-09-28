865c16ab4c5550449e54a6bab13d82442dc41789
Oleg Romadin <osromadin@gmail.com>
2024-09-29 00:46:49 +0300

    Svekis Ex1 basic done

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once './src/CroCoder/Ex4.php';
+require_once './src/Svekis/Ex1.php';
 
-use Telema\CroCoder\Ex4;
+use Telema\Svekis\Ex1;
 
-$items = Ex4::$ITEMS;
-//$result = Ex4::solution($items);
-$result = Ex4::fpSolution($items);
+$items = Ex1::$ITEMS;
+//$result = Ex1::solution($items);
+$result = Ex1::fpSolution($items);
 print_r($result);
---
+++ src/Svekis/Ex1.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Telema\Svekis;
+
+class Ex1 {
+
+	public static $ITEMS = [];
+
+	public static function solution(array $items = []) {
+
+	}
+
+	public static function fpSolution(array $items = []) {
+
+	}
+}
