16383abc84f15d2cc6b0b440f963c792286ab5d7
7006 <22399553+7006@users.noreply.github.com>
2024-09-28 10:04:17 +0300

    Update Ex1.php

--- src/Ex1.php
+++ src/Ex1.php
@@ -4,7 +4,7 @@ namespace Telema;
 
 class Ex1 {
     public static function solution(array $bucket) {
-        return [['apple', 'yellow'], ['potato', 'green']];
+        
     }
 
     public static function fp_solution() {
