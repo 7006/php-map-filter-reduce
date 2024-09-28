fcb03607c6a24790e8a8b4907aadeb07f58415f3
Oleg Romadin <osromadin@gmail.com>
2024-09-29 00:33:43 +0300

    CroCoder Ex4 foreach variant done

--- src/CroCoder/Ex4.php
+++ src/CroCoder/Ex4.php
@@ -5,9 +5,18 @@ namespace Telema\CroCoder;
 class Ex4 {
 	
 	public static $ITEMS = 'George Raymond Richard Martin';
+	// public static $ITEMS = '';
 
 	public static function solution(string $nameString = '') {
-
+		$names = explode(' ', $nameString);
+		
+		$initials = [];
+		foreach ($names as $name) {
+			$initial = $name[0];
+			$initials[] = $initial;
+		}
+		
+		return implode('', $initials);
 	}
 
 	public static function fpSolution(string $nameString = '') {
