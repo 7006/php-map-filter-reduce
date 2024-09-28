0b5c65c79c4f0ad3d0bd0194bfc9f9359dc78e54
Oleg Romadin <osromadin@gmail.com>
2024-09-29 00:09:08 +0300

    CroCoder Ex2 foreach variant done

--- src/CroCoder/Ex3.php
+++ src/CroCoder/Ex3.php
@@ -4,10 +4,50 @@ namespace Telema\CroCoder;
 
 class Ex3 {
 
-	public static $ITEMS = [];
+	//public static $ITEMS = [];
+	public static $ITEMS = [12, 46, 32, 64];
+	//public static $ITEMS = [12, 46, 32, 5, 3, 76, 64];
+	
+	public static function solution(array $numbers = []) {
+		return [
+			'mean' => self::mean($numbers),
+			'median' => self::median($numbers)
+		];
+	}
 
-	public static function solution() {
+	private static function mean(array $numbers) {
+		$sum = 0;
+		foreach ($numbers as $number) {
+			$sum += $number;
+		}
+	
+		$count = count($numbers);
 
+		if ($count === 0) {
+			$mean = 0;
+		} else {
+			$mean = $sum / $count;	
+		}
+
+		return $mean;
+	}
+
+	private static function median(array $numbers) {
+		$count = count($numbers);
+
+		if ($count === 0) {
+			return 0;
+		} 
+
+		if ($count % 2 === 0) {
+			sort($numbers);
+			return ($numbers[$count / 2 - 1] + $numbers[$count / 2]) / 2;
+		} 
+
+		if ($count % 2 === 1) {
+			sort($numbers);
+			return $numbers[($count - 1) / 2];
+		}
 	}
 
 	public static function fpSolution() {
