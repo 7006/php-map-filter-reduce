974e2fa417b5b98c258dc3762b568a5f1f293130
Oleg Romadin <osromadin@gmail.com>
2024-09-29 17:08:08 +0300

    Svekis Ex6 add some properties and new class

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once './src/Svekis/Ex4.php';
+require_once './src/Svekis/Ex6.php';
 
-use Telema\Svekis\Ex4;
+use Telema\Svekis\Ex6;
 
-$items = Ex4::$ITEMS;
-// $result = Ex4::solution($items);
-$result = Ex4::fpSolution2($items);
+$items = Ex6::$ITEMS;
+$result = Ex6::solution($items);
+// $result = Ex6::fpSolution2($items);
 print_r($result);
---
+++ src/Svekis/Ex6.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Telema\Svekis;
+
+class Rectangle {
+
+	private $width;
+	private $height;
+
+	public static function createFromArray(array $rectangle) {
+		$width = $rectangle['width'];
+		$height = $rectangle['height'];
+		return new Rectangle($width, $height);
+	}
+
+	public function __construct($width, $height) {
+		$this->width = $width;
+		$this->height = $height;
+	}
+
+	public function area() {
+		return $this->width * $this->height;
+	}
+
+	public function perimeter() {
+		return ($this->width + $this->height) * 2;	
+	}
+
+	public function longOrShort() {
+		if ($this->width > 2 * $this->height) {
+			return 'long';
+		} else {
+			return 'short';
+		}
+	}
+
+	public function favorite() {
+		if ($this->width === 1 && $this->height === 1) {
+			return 'yes';
+		}
+
+		if ($this->width === 3 && $this->height === 3) {
+			return 'yes';
+		}
+
+		if ($this->width === 10 && $this->height === 2) {
+			return 'i dont know';
+		}
+
+		return 'no';
+	}
+}
+
+class Ex6 {
+
+	public static $ITEMS = [
+		[
+			'width' => 15,
+			'height' => 5
+		],
+		[
+			'width' => 4,
+			'height' => 8
+		],
+		[
+			'width' => 10,
+			'height' => 2
+		],
+		[
+			'width' => 2,
+			'height' => 6
+		],
+		[
+			'width' => 1,
+			'height' => 1
+		]
+	];
+
+	public static function solution(array $rectangles = []) {
+		$infos = [];
+		foreach ($rectangles as $rectangleArray) {
+			$rectangleObject = Rectangle::createFromArray($rectangleArray);
+			$info = [
+				'area' => $rectangleObject->area(),
+				'perimeter' => $rectangleObject->perimeter(),
+				'long_or_short' => $rectangleObject->longOrShort(),
+				'favorite' => $rectangleObject->favorite()
+			];
+			$infos[] = $info;
+		}
+		
+		return $infos;
+	}
+
+	public static function fpSolution(array $rectangles = []) {
+		
+	}
+}
