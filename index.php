7cf8f60959d6a5dd7bb918435a932a0fe4ff39bf
Oleg Romadin <osromadin@gmail.com>
2024-10-05 23:32:41 +0300

    add function go

--- index.php
+++ index.php
@@ -2,18 +2,18 @@
 
 require_once './vendor/autoload.php';
 
-$requestUri = $_SERVER['REQUEST_URI'];
-
-if (str_starts_with($requestUri, '/crocoder/')) {
-	$url = substr($requestUri, strlen('/crocoder/'));
-	$result = \Telema\CroCoder\Router::go($url);
+function go($prefix, $class) {
+	$requestUri = $_SERVER['REQUEST_URI'];
+	
+	if (str_starts_with($requestUri, $prefix)) {
+		$url = substr($requestUri, strlen($prefix));
+		$result = $class::go($url);
+		
+		echo '<pre>';
+		print_r($result);
+		echo '</pre>';
+	}
 }
 
-if (str_starts_with($requestUri, '/svekis/')) {
-	$url = substr($requestUri, strlen('/svekis/'));
-	$result = \Telema\Svekis\Router::go($url);
-}
-
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+go('/crocoder/', \Telema\CroCoder\Router::class);
+go('/svekis/', \Telema\Svekis\Router::class);
