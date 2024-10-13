bbaa7ab53266f8fa01682c01558244ddf9237c6a
Oleg Romadin <osromadin@gmail.com>
2024-10-13 21:19:23 +0300

    Uncle Ex3 update

---
+++ src/Uncle/Ex3.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Telema\Uncle;
+
+const ITEMS = [
+    ['id' => 22, 'provider' => 'yolo', 'updated_at' => '2024-09-28'],
+    ['id' => 10, 'provider' => 'frob', 'updated_at' => '2024-10-01'],
+    ['id' => 34, 'provider' => 'yolo', 'updated_at' => '2024-09-28'],
+    ['id' => 43, 'provider' => 'boom', 'updated_at' => '2024-09-03'],
+    ['id' => 26, 'provider' => 'frob', 'updated_at' => '2024-09-02'],
+    ['id' => 34, 'provider' => 'boom', 'updated_at' => '2024-09-20'],
+    ['id' => 43, 'provider' => 'boom', 'updated_at' => '2024-09-03'],
+    ['id' => 26, 'provider' => 'frob', 'updated_at' => '2024-09-04']
+];
+
+class Ex2 {
+
+	public static function solution(array $items = ITEMS) {
+
+}
+
+	public static function fpSolution(array $items = ITEMS) {
+
+}
