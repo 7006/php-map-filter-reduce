8b2f327885b385ab63c8b0a0c99bc883adfc0814
7006 <22399553+7006@users.noreply.github.com>
2024-10-13 22:53:29 +0300

    Update Ex5.php

--- src/CroCoder/Ex5.php
+++ src/CroCoder/Ex5.php
@@ -31,21 +31,8 @@ const ITEMS = [
 class Ex5 {
 
 	public static function solution(array $items = ITEMS) {
-		$result = [];
-		foreach ($items as $item) {
-			$result[] = $item ** 2;
-		}
-		return $result;
 	}
 
 	public static function fpSolution(array $items = ITEMS) {
-		return array_map(fn ($item) => $item ** 2, $items);
-	}
-
-	public static function fpSolution7(array $items = ITEMS) {
-		$callback = function($item) {
-			return $item ** 2;
-		};
-		return array_map($callback, $items);
 	}
 }
