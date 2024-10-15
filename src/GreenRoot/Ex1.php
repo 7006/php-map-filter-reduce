9b49255a4cf9fe66ee808cdab9030e6d6e6a3bbc
7006 <22399553+7006@users.noreply.github.com>
2024-10-15 22:58:43 +0300

    Update Ex1.php

--- src/GreenRoot/Ex1.php
+++ src/GreenRoot/Ex1.php
@@ -1,1 +1,19 @@
 
+<?php
+
+namespace Telema\Uncle;
+
+class Ex1 {
+
+	public static function solution(array $customers) {
+		
+	}
+
+	public static function fpSolution(array $customers) {
+		
+	}
+
+	private static function color($food) {
+		
+	}
+}
