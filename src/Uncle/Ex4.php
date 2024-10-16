23ef45c5fba7830154d0602aace7ae673e874e04
Oleg Romadin <osromadin@gmail.com>
2024-10-16 15:50:22 +0300

    Uncle Ex4 foreach first variant done

--- src/Uncle/Ex4.php
+++ src/Uncle/Ex4.php
@@ -13,7 +13,7 @@ const ITEMS = [
     ['id' => 26, 'provider' => 'frob']
 ];
 
-const RATINGS = [
+const ITEMS_2 = [
     ['provider' => 'yolo', 'rating' => 0.7],
     ['provider' => 'frob', 'rating' => 0.2],
     ['provider' => 'boom', 'rating' => 0.5]
@@ -25,11 +25,33 @@ class Ex4 {
 		return empty($_GET['rating']) ? null : $_GET['rating'];
 	}
 
-	public static function solution(array $items = ITEMS) {
-		echo self::rating();
+	public static function solution(array $items = ITEMS, array $items2 = ITEMS_2) {
+		$receivedRating = self::rating();
+		$ratings = [];
+		$result = [];
+
+		foreach ($items2 as $item2) {
+			if ($item2['rating'] >= $receivedRating) {
+				$ratings[] = $item2;
+			}
+		}
+
+		foreach ($items as $item) {
+			foreach ($ratings as $rating) {
+				if ($item['provider'] === $rating['provider']) {
+					$result[] = $item;
+				}
+			}
+		}
+		
+		// echo '<pre>';
+		// print_r($result);
+		// echo '</pre>';
+
+		return $result;
 	}
 
-	public static function fpSolution(array $items = ITEMS) {
+	public static function fpSolution(array $items = ITEMS, array $ratings = ITEMS_2) {
 		
 	}
 }
