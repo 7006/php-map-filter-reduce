bdf7988cf7ca152e2ab71f4af622a1413e1e7ae7
Oleg Romadin <osromadin@gmail.com>
2024-10-16 19:25:14 +0300

    GreenRoot Ex1 foreach variant done

--- src/GreenRoot/Ex1.php
+++ src/GreenRoot/Ex1.php
@@ -3,15 +3,25 @@
 namespace Telema\GreenRoot;
 
 class Ex1 {
+	
 	private static function readCustomers() {
-		// код которьІй читает из файла и декодирует JSON
+		$customersJson = file_get_contents(__DIR__ . '/customers.json');
+		return json_decode($customersJson, true);
 	}
 
 	public static function solution() {
 		$customers = self::readCustomers();
-		echo 1;
-		// код которьІй фильтрует - преобразовьІвает ...
+		$seniorCitizens = [];
+
+		foreach ($customers as $customer) {
+			if ($customer['age'] > 60) {
+				$seniorCitizens[] = $customer;		
+			}
+		}
+
+		return $seniorCitizens;
 	}
+
 
 	public static function fpSolution() {
 		
