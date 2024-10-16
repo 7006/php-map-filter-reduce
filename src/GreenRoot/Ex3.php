dd15f2bb3a6f258cf89dc298b14a20a46ccb9c19
Oleg Romadin <osromadin@gmail.com>
2024-10-16 23:41:02 +0300

    GreenRoot Ex3 update foreach

--- src/GreenRoot/Ex3.php
+++ src/GreenRoot/Ex3.php
@@ -4,8 +4,21 @@ namespace Telema\GreenRoot;
 
 class Ex3 {
 
-	public static function solution() {
+	private static function readCustomers() {
+		return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
+	}
 
+	public static function solution() {
+		$sum = 0;
+		$count = 0;
+
+		foreach (self::readCustomers() as $customer) {
+			if (in_array('Book', $customer['purchased'])) {
+				$sum += $customer['age'];
+				++$count;		
+			}
+		}
+		return $sum / $count;
 	}
 
 	public static function fpSolution() {
