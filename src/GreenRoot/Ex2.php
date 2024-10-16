c4ed3eea22939d51caa9c64660e6bd575198d395
Oleg Romadin <osromadin@gmail.com>
2024-10-16 21:47:05 +0300

    GreenRoot Ex2 done

--- src/GreenRoot/Ex2.php
+++ src/GreenRoot/Ex2.php
@@ -8,45 +8,40 @@ class Ex2 {
 		return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
 	}
 
-	private static function addFullName($title) {
-		$customer['full_name'] = $title . $customer['f_name'] . $customer['l_name'];
+	private static function title($customer) {
+		if ($customer['gender'] === 'M') {
+			return 'Mr.';
+		}
+		if ($customer['gender'] === 'F' && $customer['married']) {
+			return 'Mrs.';
+		}	
+		if ($customer['gender'] === 'F' && !$customer['married']) {
+			return 'Miss.';
+		}
+	}
+
+	private static function fullName($customer) {
+		return $customer['title'] . ' ' . $customer['f_name'] . ' ' . $customer['l_name'];
 	}
 
 	public static function solution() {
-		$customers = self::readCustomers();
+		$customers = [];
 
-		foreach ($customers as &$customer) {
-
-			//match
-			
-			if($customer['gender'] === 'M') {
-				$title = 'Mr.';
-				$customer['title'] = $title;
-				$customer['full_name'] = self::addFullName($title);
-			}
-
-			if ($customer['gender'] === 'F' && $customer['married'] == true) {
-				$title = 'Mrs.';
-				$customer['title'] = $title;
-				$customer['full_name'] = self::addFullName($title);
-			}	
-			
-			if ($customer['gender'] === 'F' && $customer['married'] == false) {
-				$title = 'Miss.';
-				$customer['title'] = $title;
-				$customer['full_name'] = self::addFullName($title);
-			}
+		foreach (self::readCustomers() as $customer) {
+			$customer['title'] = self::title($customer);
+			$customer['full_name'] = self::fullName($customer);
+			$customers[] = $customer;
 		}
 		
 		return $customers;
-
-		// echo '<pre>';
-		// print_r($customers);
-		// echo '</pre>';
-
 	}
 
 	public static function fpSolution() {
-		
+		$fn	= function ($customer) {
+			$customer['title'] = self::title($customer);
+			$customer['full_name'] = self::fullName($customer);
+			return $customer;
+		};
+		return array_map($fn, self::readCustomers());
 	}
 }
