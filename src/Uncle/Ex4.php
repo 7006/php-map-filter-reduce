67f5799d82bd7518659c9ee03dcbaf54dfd93b70
Oleg Romadin <osromadin@gmail.com>
2024-10-16 15:06:59 +0300

    Uncle Ex4 basic

---
+++ src/Uncle/Ex4.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Telema\Uncle;
+
+const ITEMS = [
+    ['id' => 22, 'provider' => 'yolo'],
+    ['id' => 10, 'provider' => 'frob'],
+    ['id' => 34, 'provider' => 'yolo'],
+    ['id' => 43, 'provider' => 'boom'],
+    ['id' => 26, 'provider' => 'frob'],
+    ['id' => 34, 'provider' => 'boom'],
+    ['id' => 43, 'provider' => 'boom'],
+    ['id' => 26, 'provider' => 'frob']
+];
+
+const RATINGS = [
+    ['provider' => 'yolo', 'rating' => 0.7],
+    ['provider' => 'frob', 'rating' => 0.2],
+    ['provider' => 'boom', 'rating' => 0.5]
+];
+
+class Ex4 {
+
+	public static function solution(array $items = ITEMS) {
+
+	}
+
+	public static function fpSolution(array $items = ITEMS) {
+		
+	}
+}
