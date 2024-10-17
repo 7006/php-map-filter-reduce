c79f27e3052effbcef9f6d08a02698f7021a23d5
Oleg Romadin <osromadin@gmail.com>
2024-10-17 18:35:30 +0300

    GreenRoot Ex5 foreach solution done

--- src/GreenRoot/Ex5.php
+++ src/GreenRoot/Ex5.php
@@ -4,9 +4,20 @@ namespace Telema\GreenRoot;
 
 class Ex5 {
 
-	public static function solution() {
-
+	private static function readCustomers() {
+		return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
 	}
+
+	public static function solution() {
+		$youngestCustomers = [];
+
+		foreach (self::readCustomers() as $customer) {
+			if ($customer['age'] < 10) {
+				$youngestCustomers[] = $customer;				
+			}
+		}
+		return $youngestCustomers;
+}
 
 	public static function fpSolution() {
 		
