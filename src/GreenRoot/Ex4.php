6a9c0e879ee50ce9e630e78560d87a14567efde4
Oleg Romadin <osromadin@gmail.com>
2024-10-17 18:19:01 +0300

    GreenRoot Ex4 done

--- src/GreenRoot/Ex4.php
+++ src/GreenRoot/Ex4.php
@@ -4,11 +4,27 @@ namespace Telema\GreenRoot;
 
 class Ex4 {
 
-	public static function solution() {
+	private static function readCustomers() {
+		return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
+	}
 
+	public static function solution() {
+		$doWeHaveYoungCustomers = 'No';
+
+		foreach (self::readCustomers() as $customer) {
+			If ($customer['age'] < 10) {
+				$doWeHaveYoungCustomers = 'Yes';
+			}
+		}
+		
+		return $doWeHaveYoungCustomers;	
 	}
 
 	public static function fpSolution() {
-		
+		return array_reduce(
+			self::readCustomers(),
+			fn ($message, $customer) => ($customer['age'] < 10 ? 'Yes' : 'No'),
+			'No'
+		);
 	}
 }
