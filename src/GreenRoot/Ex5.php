3b6cf1067f6947a9522138a26c2aa5cdd44ad7bb
Oleg Romadin <osromadin@gmail.com>
2024-10-18 18:30:26 +0300

    fix Ex5 by comments. delete unnecessary functions

--- src/GreenRoot/Ex5.php
+++ src/GreenRoot/Ex5.php
@@ -8,67 +8,26 @@ class Ex5 {
 		return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
 	}
 
-	// вот тут грамотно
 	private static function sortByAge($customers) {
 		usort($customers, fn ($cc, $nc) => $cc['age'] <=> $nc['age']);
-		return array_values($customers); // разве usort не сбрасьівает индексьі сам ?
-	}
-
-	private static function getLowerAge($sortedCustomers) {
-		// в 0 индексе как раз и находится самьІй младший
-		// потому что список отсортирован
-		return $sortedCustomers[0]['age'];
-	}
-
-	private static function getUpperAge($sortedCustomers) {
-		// в последнем индексе как раз и находится самьІй старший
-		// потому что список отсортирован
-		return $sortedCustomers[count($sortedCustomers) - 1]['age'];
-	}
-
-	// єта функция не нужна
-	// потому что мьІ уже нашли самого младшего и самого старшего
-	// после того как осортировали список
-	// но так как мЬІ вернули не ассоц массив - покупатель - а его возраст
-	// то приходится фильтровать чтобьТ найти ассоц массивьІ
-	// которьІе потворюсь нашли пошли сортировки
-	private static function getYoungestAndOldestCustomers($sortedCustomers) {
-		$youngestAndOldestCustomers =[];
-		$lowerAge = self::getLowerAge($sortedCustomers);
-		$upperAge = self::getUpperAge($sortedCustomers);
-
-		$youngestAndOldestCustomers[] = array_filter(
-			$sortedCustomers,
-			fn ($customer) => $customer['age'] === $lowerAge || $customer['age'] === $upperAge
-		);
-		
-		return $youngestAndOldestCustomers;
+		return $customers;
 	}
 
 	public static function solution() {
-		$youngestCustomers = [];
-
 		foreach (self::readCustomers() as $customer) {
 			if ($customer['age'] < 10) {
-				$youngestCustomers[] = $customer; //  сразу вернуть тогда $youngestCustomers не нужно		
+				return $customer;		
 			}
 		}
-		return $youngestCustomers;
-	}
-
-	public static function fpSolution2() {
-		// оно найдет всех покупателей младше 10 
-		// их может бьІть несколько
-		// нужен самьІй младший
-		// фильтр никак не поможет єто сделать
-		// для єтого нужна сортировка 
-		return array_filter(self::readCustomers(), fn ($customer) => $customer['age'] < 10); 
 	}
 
 	public static function fpSolution() {
+		$youngestAndOldestCustomers =[];
 		$customers = self::readCustomers();
-		$sortedCustomers = self::sortByAge($customers); // єто грамотно
-		$youngestAndOldestCustomers = self::getYoungestAndOldestCustomers($sortedCustomers); // єто нет
+		$sortedCustomers = self::sortByAge($customers);
+		
+		$youngestAndOldestCustomers[] = $sortedCustomers[0];
+		$youngestAndOldestCustomers[] = $sortedCustomers[count($sortedCustomers) - 1];
 		
 		return $youngestAndOldestCustomers;
 	}
