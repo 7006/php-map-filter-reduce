e4c993e375de744c9e18f3ac353f09dc78b67b8d
7006 <22399553+7006@users.noreply.github.com>
2024-10-18 09:19:54 +0300

    Update Ex5.php

--- src/GreenRoot/Ex5.php
+++ src/GreenRoot/Ex5.php
@@ -8,19 +8,30 @@ class Ex5 {
 		return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
 	}
 
+	// вот тут грамотно
 	private static function sortByAge($customers) {
 		usort($customers, fn ($cc, $nc) => $cc['age'] <=> $nc['age']);
-		return array_values($customers);
+		return array_values($customers); // разве usort не сбрасьівает индексьі сам ?
 	}
 
 	private static function getLowerAge($sortedCustomers) {
+		// в 0 индексе как раз и находится самьІй младший
+		// потому что список отсортирован
 		return $sortedCustomers[0]['age'];
 	}
 
 	private static function getUpperAge($sortedCustomers) {
+		// в последнем индексе как раз и находится самьІй старший
+		// потому что список отсортирован
 		return $sortedCustomers[count($sortedCustomers) - 1]['age'];
 	}
 
+	// єта функция не нужна
+	// потому что мьІ уже нашли самого младшего и самого старшего
+	// после того как осортировали список
+	// но так как мЬІ вернули не ассоц массив - покупатель - а его возраст
+	// то приходится фильтровать чтобьТ найти ассоц массивьІ
+	// которьІе потворюсь нашли пошли сортировки
 	private static function getYoungestAndOldestCustomers($sortedCustomers) {
 		$youngestAndOldestCustomers =[];
 		$lowerAge = self::getLowerAge($sortedCustomers);
@@ -39,20 +50,25 @@ class Ex5 {
 
 		foreach (self::readCustomers() as $customer) {
 			if ($customer['age'] < 10) {
-				$youngestCustomers[] = $customer;				
+				$youngestCustomers[] = $customer; //  сразу вернуть тогда $youngestCustomers не нужно		
 			}
 		}
 		return $youngestCustomers;
 	}
 
 	public static function fpSolution2() {
-		return array_filter(self::readCustomers(), fn ($customer) => $customer['age'] < 10);
+		// оно найдет всех покупателей младше 10 
+		// их может бьІть несколько
+		// нужен самьІй младший
+		// фильтр никак не поможет єто сделать
+		// для єтого нужна сортировка 
+		return array_filter(self::readCustomers(), fn ($customer) => $customer['age'] < 10); 
 	}
 
 	public static function fpSolution() {
 		$customers = self::readCustomers();
-		$sortedCustomers = self::sortByAge($customers);
-		$youngestAndOldestCustomers = self::getYoungestAndOldestCustomers($sortedCustomers);
+		$sortedCustomers = self::sortByAge($customers); // єто грамотно
+		$youngestAndOldestCustomers = self::getYoungestAndOldestCustomers($sortedCustomers); // єто нет
 		
 		return $youngestAndOldestCustomers;
 	}
