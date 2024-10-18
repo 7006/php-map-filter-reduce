9f796f39ce7d9fb6cad1887f37a7d71aeccdce85
Oleg Romadin <osromadin@gmail.com>
2024-10-18 19:46:36 +0300

    GrennRoot Ex6 done

--- src/GreenRoot/Ex6.php
+++ src/GreenRoot/Ex6.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Telema\GreenRoot;
+
 class Ex6 {
 	
 	private static function readCustomers() {
@@ -7,9 +9,18 @@ class Ex6 {
 	}
 
 	public static function solution() {
+		$customersWitoutPurchase = [];
+		
+		foreach (self::readCustomers() as $customer) {
+			if (empty($customer['purchased'])) {
+				$customersWitoutPurchase[] = $customer;
+			}
+		}
+
+		return $customersWitoutPurchase; 
 	}
 
 	public static function fpSolution() {
-		
+		return array_filter(self::readCustomers(), fn ($customer) => empty($customer['purchased'])); 
 	}
 }
