9a89dfe10b812ff35e68339388fd52a0601e5c6d
Oleg Romadin <osromadin@gmail.com>
2024-10-19 19:38:11 +0300

    GreenRoot Ex3 fixed by comments. add new solution

---
+++ src/CroCoder/Ex6.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Telema\CroCoder;
+
+class Ex5
+{
+ 	public static function solution(array $items = ITEMS) {
+
+    }
+
+    public static function fpSolution(array $items = ITEMS) {
+    	
+    }
+}
--- src/GreenRoot/Ex3.php
+++ src/GreenRoot/Ex3.php
@@ -14,59 +14,53 @@ class Ex3
         return in_array('Book', $customer['purchased']);
     }
 
-    private static function isZero($count)
+    private static function avgAge($totalAge, $count)
     {
-        if ($count === 0) {
-            echo 'Division by zero';
+        try {
+            return intdiv($totalAge, $count);
+        } catch (\DivisionByZeroError $e) {
+            return 0;
         }
     }
 
     public static function solution()
     {
-        $sum = 0;
+        $totalAge = 0;
         $count = 0;
 
         foreach (self::readCustomers() as $customer) {
             if (self::isCustomerPurchasedBook($customer)) {
-                $sum += $customer['age'];
+                $totalAge += $customer['age'];
                 $count += 1;
             }
         }
 
-        // єто какая-то дічь
-        self::isZero($count);
-        return intdiv($sum, $count);
-        // конец дічі
+        return self::avgAge($totalAge, $count);
     }
 
     public static function fpSolution2()
     {
         $customersWhoBoughtBook = array_filter(
             self::readCustomers(),
-            fn ($customer) => in_array('Book', $customer['purchased'])
+            self::isCustomerPurchasedBook(...)
         );
 
-        $count = array_reduce(
-            $customersWhoBoughtBook,
-            fn ($count, $customer) => $count + 1,
-            0
-        );
+        $count = count($customersWhoBoughtBook);
 
-        $sum = array_reduce(
+        $totalAge = array_reduce(
             $customersWhoBoughtBook,
             fn ($age, $customer) => $age + $customer['age'],
             0
         );
 
-        self::isZero($count);
-        return intdiv($sum, $count);
+        return self::avgAge($totalAge, $count);
     }
 
-    public static function fpSolution()
+    public static function fpSolution3()
     {
         $fn = function ($accum, $customer) {
             if (self::isCustomerPurchasedBook($customer)) {
-                $accum['age'] = $accum['age'] + $customer['age'];
+                $accum['age'] += $customer['age'];
                 $accum['count'] += 1;
             }
             return $accum;
@@ -80,8 +74,25 @@ class Ex3
                 'count' => 0
             ]
         );
-        -
-                self::isZero($accum['count']);
-        return intdiv($accum['age'], $accum['count']);
+
+        return self::avgAge($accum['age'], $accum['count']);
     }
+
+    public static function fpSolution() {
+
+        $fn = function ($accum, $customer) {
+            [$age, $count] = $accum;
+            
+            if (self::isCustomerPurchasedBook($customer)) {
+                $age += $customer['age'];
+                $count += 1;
+            }
+            
+            return [$age, $count];
+        };
+     
+        [$age, $count] = array_reduce(self::readCustomers(), $fn, [0, 0]);
+        
+        return self::avgAge($age, $count);
+    }   
 }
