03823455c880d445786c72a151a849b47457d974
Oleg Romadin <osromadin@gmail.com>
2024-10-19 20:07:14 +0300

    relocate title() and fullname()

--- src/GreenRoot/Customer.php
+++ src/GreenRoot/Customer.php
@@ -8,4 +8,30 @@ class Customer {
     {
         return json_decode(file_get_contents(__DIR__ . '/customers.json'), true);
     }
+
+    public static function title($customer)
+    {
+
+        $gender = $customer['gender'];
+        $married = $customer['married'];
+
+        if ($gender === 'M') {
+            return 'Mr.';
+        }
+
+        if ($gender === 'F' && $married) {
+            return 'Mrs.';
+        }
+
+        if ($gender === 'F' && !$married) {
+            return 'Miss.';
+        }
+
+        return '';
+    }
+
+    public static function fullName($customer)
+    {
+        return $customer['title'] . ' ' . $customer['f_name'] . ' ' . $customer['l_name'];
+    }
 }
--- src/GreenRoot/Ex2.php
+++ src/GreenRoot/Ex2.php
@@ -4,39 +4,13 @@ namespace Telema\GreenRoot;
 
 class Ex2
 {
-    private static function title($customer)
-    {
-
-        $gender = $customer['gender'];
-        $married = $customer['married'];
-
-        if ($gender === 'M') {
-            return 'Mr.';
-        }
-
-        if ($gender === 'F' && $married) {
-            return 'Mrs.';
-        }
-
-        if ($gender === 'F' && !$married) {
-            return 'Miss.';
-        }
-
-        return '';
-    }
-
-    private static function fullName($customer)
-    {
-        return $customer['title'] . ' ' . $customer['f_name'] . ' ' . $customer['l_name'];
-    }
-
     public static function solution()
     {
         $customers = [];
 
         foreach (Customer::readCustomers() as $customer) {
-            $customer['title'] = self::title($customer);
-            $customer['full_name'] = self::fullName($customer);
+            $customer['title'] = Customer::title($customer);
+            $customer['full_name'] = Customer::fullName($customer);
             $customers[] = $customer;
         }
 
@@ -46,8 +20,8 @@ class Ex2
     public static function fpSolution()
     {
         $fn	= function ($customer) {
-            $customer['title'] = self::title($customer);
-            $customer['full_name'] = self::fullName($customer);
+            $customer['title'] = Customer::title($customer);
+            $customer['full_name'] = Customer::fullName($customer);
             return $customer;
         };
         return array_map($fn, Customer::readCustomers());
