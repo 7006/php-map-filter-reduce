4063d741ecdbbde748ba1924ad0aa804989179e8
Oleg Romadin <osromadin@gmail.com>
2024-10-21 17:50:32 +0300

    CroCoder Ex5 foreach solution done

--- src/CroCoder/Ex5.php
+++ src/CroCoder/Ex5.php
@@ -4,6 +4,10 @@ namespace Telema\CroCoder;
 
 const ITEMS = [
     [
+        'name' => 'Vasiliy',
+        'age' => 34
+    ],
+    [
         'name' => 'Jhon',
         'age' => 13
     ],
@@ -29,6 +33,19 @@ class Ex5
 {
     public static function solution(array $items = ITEMS)
     {
+        $youngest = $items[0]['age'];
+        $oldest = $items[0]['age'];
+
+        foreach ($items as $item) {
+            if ($item['age'] < $youngest) {
+                $youngest = $item['age'];
+            }
+
+            if ($item['age'] > $oldest) {
+                $oldest = $item['age'];
+            }
+        }
+        return [$youngest, $oldest, $oldest - $youngest];
     }
 
     public static function fpSolution(array $items = ITEMS)
