ca2afb1396a38274c9548189eb9a45bcf1662a3c
Oleg Romadin <osromadin@gmail.com>
2024-10-25 21:51:33 +0300

    explode request, check length, get class, methods names

--- index.php
+++ index.php
@@ -9,11 +9,39 @@ function go($prefix, $class) {
 	if (str_starts_with($requestUri, $prefix)) {
 		$url = substr($requestUri, strlen($prefix));
 		$result = $class::go($url);
-		
+
+		newRouter($requestUri);
 		echo '<pre>';
 		print_r($result);
 		echo '</pre>';
 	}
+}
+
+function newRouter($requestUri) {
+	
+	var_dump($requestUri);
+	
+	$requests = explode('/', $requestUri);
+	echo '<pre>';
+	print_r($requests);
+	echo '</pre>';
+
+	if (count($requests) === 3) {
+		$className = 'Ex' . $requests[2];
+		$methodName = 'solution()';
+	}
+
+	if (count($requests) === 4) {
+		$className = 'Ex' . $requests[3];
+		$methodName = 'fpSolution()';
+	}
+
+	$root = ucfirst($requests[1]);
+	$namespace = "\Telema\\$root\\";
+
+	var_dump($root, $namespace, $className, $methodName);
+
+	
 }
 
 go('/crocoder/', \Telema\Crocoder\Router::class);
