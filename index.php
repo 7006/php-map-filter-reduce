31c354b6c8ce4204b886c41d5874db08a286b3c7
Oleg Romadin <osromadin@gmail.com>
2024-10-25 23:09:16 +0300

    index router works

--- index.php
+++ index.php
@@ -2,49 +2,17 @@
 
 require_once './vendor/autoload.php';
 
-function go($prefix, $class) {
+$parts = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 
-	$requestUri = $_SERVER['PATH_INFO'];
-	
-	if (str_starts_with($requestUri, $prefix)) {
-		$url = substr($requestUri, strlen($prefix));
-		$result = $class::go($url);
+[$class, $method] = match (count($parts)) {
+	2 => [sprintf('Telema\%s\Ex%s', ucfirst($parts[0]), $parts[1]), 'solution'],
+	3 => [sprintf('Telema\%s\Ex%s', ucfirst($parts[0]), $parts[2]), 'fpSolution'],
+	default => ['', '']
+};
 
-		newRouter($requestUri);
-		echo '<pre>';
-		print_r($result);
-		echo '</pre>';
-	}
+if (method_exists($class, $method)) {
+	$returnValue = call_user_func([$class, $method]);
+	printf('<pre>%s</pre>', print_r($returnValue, true));
+} else {
+	printf('Class %s and/or method %s not found', $class, $method);
 }
-
-function newRouter($requestUri) {
-	
-	var_dump($requestUri);
-	
-	$requests = explode('/', $requestUri);
-	echo '<pre>';
-	print_r($requests);
-	echo '</pre>';
-
-	if (count($requests) === 3) {
-		$className = 'Ex' . $requests[2];
-		$methodName = 'solution()';
-	}
-
-	if (count($requests) === 4) {
-		$className = 'Ex' . $requests[3];
-		$methodName = 'fpSolution()';
-	}
-
-	$root = ucfirst($requests[1]);
-	$namespace = "\Telema\\$root\\";
-
-	var_dump($root, $namespace, $className, $methodName);
-
-	
-}
-
-go('/crocoder/', \Telema\Crocoder\Router::class);
-go('/svekis/', \Telema\Svekis\Router::class);
-go('/uncle/', \Telema\Uncle\Router::class);
-go('/greenroot/', \Telema\Greenroot\Router::class);
