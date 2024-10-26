ee1cf5365c12c0c1b71a5f00e0214275e74a1d0d
o.medianyk <o.medianyk@favbet.tech>
2024-10-26 09:54:36 +0300

    Code format

--- index.php
+++ index.php
@@ -5,15 +5,15 @@ require_once './vendor/autoload.php';
 $parts = explode('/', trim($_SERVER['PATH_INFO'], '/'));
 
 [$class, $method] = match (count($parts)) {
-	2 => [sprintf('Telema\%s\Ex%s', ucfirst($parts[0]), $parts[1]), 'solution'],
-	3 => [sprintf('Telema\%s\Ex%s', ucfirst($parts[0]), $parts[2]), 'fpSolution'],
-	default => ['', '']
+    2 => [sprintf('Telema\%s\Ex%s', ucfirst($parts[0]), $parts[1]), 'solution'],
+    3 => [sprintf('Telema\%s\Ex%s', ucfirst($parts[0]), $parts[2]), 'fpSolution'],
+    default => ['', '']
 };
 
 if (method_exists($class, $method)) {
-	$returnValue = call_user_func([$class, $method]);
-	// $returnValue = $class::$method(...)();
-	printf('<pre>%s</pre>', print_r($returnValue, true));
+    $returnValue = call_user_func([$class, $method]);
+    // $returnValue = $class::$method(...)();
+    printf('<pre>%s</pre>', print_r($returnValue, true));
 } else {
-	printf('Class %s and/or method %s not found', $class, $method);
+    printf('Class %s and/or method %s not found', $class, $method);
 }
