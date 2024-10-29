754b9c76f2ba143edffe9f59530bab9d07bf5acf
o.medianyk <o.medianyk@favbet.tech>
2024-10-29 18:46:27 +0200

    Code format

--- src/Crocoder/Ex7.php
+++ src/Crocoder/Ex7.php
@@ -4,14 +4,15 @@ namespace Telema\Crocoder;
 
 class Ex7
 {
-	public const NUMBER = 6;
+    public const NUMBER = 6;
 
-	public function __invoke() {
-		$factorial = 1;
+    public function __invoke()
+    {
+        $factorial = 1;
 
-		foreach (range(1, self::NUMBER) as $number) {
-			$factorial *= $number;
-		}
-		return $factorial;
-	}
+        foreach (range(1, self::NUMBER) as $number) {
+            $factorial *= $number;
+        }
+        return $factorial;
+    }
 }
