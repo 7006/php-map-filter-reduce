06cf2bc2e22ea8636f9b1a44b9f21d76e416b5b3
Oleg Romadin <osromadin@gmail.com>
2024-10-30 15:38:13 +0200

    crocoder9 fix2

--- src/Crocoder/Fp/Ex9.php
+++ src/Crocoder/Fp/Ex9.php
@@ -7,40 +7,13 @@ use Telema\Math;
 
 class Ex9 extends BaseEx9
 {	
-		// public function __invoke() {
-
-		// 	$fn = function ($item) {
-		// 		return [
-		// 			'name' => $item['name'],
-		// 			'average' => $item['scores']
-		// 		];			
-		// 	};
-			
-		// 	$items = array_map($fn, self::ITEMS);
-
-		// 	// echo '<pre>';
-		// 	// print_r($items);
-		// 	// echo '</pre>';
-
-		// 	return array_filter(
-		// 		$items,
-		// 		fn($item) => Math::avg(array_sum($item['average']), count($item['average'])) > self::AVARAGE_SCORE
-		// 	);
-		// }
-
 	public function __invoke() {
 
 		$items = array_filter(
 			self::ITEMS,
-			fn($item) => Math::avg(array_sum($item['scores']), count($item['scores'])) > self::AVARAGE_SCORE
+			fn($item) => Math::avg(array_sum($item['scores']), count($item['scores'])) > self::AVERAGE_SCORE
 		);
 
-		// $fn = function ($item) {
-		// 	return [
-				
-		// 	];			
-		// };
-		
 		return array_map(
 			fn ($item) => [
 				'name' => $item['name'],
@@ -48,9 +21,5 @@ class Ex9 extends BaseEx9
 			],
 		 	array_values($items)
 		);
-
-		
-
-		
 	}
 }
