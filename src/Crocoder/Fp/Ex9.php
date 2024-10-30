1ea18e8564af9ac6d268873451329aec6c8c516a
Oleg Romadin <osromadin@gmail.com>
2024-10-30 15:35:36 +0200

    fix by comment

--- src/Crocoder/Fp/Ex9.php
+++ src/Crocoder/Fp/Ex9.php
@@ -7,16 +7,50 @@ use Telema\Math;
 
 class Ex9 extends BaseEx9
 {	
+		// public function __invoke() {
+
+		// 	$fn = function ($item) {
+		// 		return [
+		// 			'name' => $item['name'],
+		// 			'average' => $item['scores']
+		// 		];			
+		// 	};
+			
+		// 	$items = array_map($fn, self::ITEMS);
+
+		// 	// echo '<pre>';
+		// 	// print_r($items);
+		// 	// echo '</pre>';
+
+		// 	return array_filter(
+		// 		$items,
+		// 		fn($item) => Math::avg(array_sum($item['average']), count($item['average'])) > self::AVARAGE_SCORE
+		// 	);
+		// }
+
 	public function __invoke() {
 
-		$fn = function ($item) {
-			$item['scores'] = array_sum($item['scores']);
-			return array_combine(['name', 'avarage'], $item);
-		};
-		
-		return array_filter(
-			array_map($fn, self::ITEMS),
-			fn($item) => Math::avg($item['avarage'], 3) > 90
+		$items = array_filter(
+			self::ITEMS,
+			fn($item) => Math::avg(array_sum($item['scores']), count($item['scores'])) > self::AVARAGE_SCORE
 		);
+
+		// $fn = function ($item) {
+		// 	return [
+				
+		// 	];			
+		// };
+		
+		return array_map(
+			fn ($item) => [
+				'name' => $item['name'],
+				'average' => array_sum($item['scores'])	
+			],
+		 	array_values($items)
+		);
+
+		
+
+		
 	}
 }
