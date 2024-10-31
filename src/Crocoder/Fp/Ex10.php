166d880de0fe3acd6f3db6b458baa330057db3c2
Oleg Romadin <osromadin@gmail.com>
2024-10-31 20:17:39 +0200

    group by categories

--- src/Crocoder/Fp/Ex10.php
+++ src/Crocoder/Fp/Ex10.php
@@ -3,11 +3,17 @@
 namespace Telema\Crocoder\Fp;
 
 use Telema\Math;
-use Telema\Crocoder\Ex10 as BaseEx10
+use Telema\Crocoder\Ex10 as BaseEx10;
 
 class Ex10 extends BaseEx10
 {
 	public function __invoke() {
-		
+			
+		$fn = function ($categories, $product) {
+			$categories[$product['category']][] = $product;
+			return $categories;
+		};
+
+		return array_reduce(self::PRODUCTS, $fn, []);
 	}
 }
