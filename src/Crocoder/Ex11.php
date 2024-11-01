9aec0d53be17af9ef01c1eb2fffb52caeeabb824
o.medianyk <o.medianyk@favbet.tech>
2024-11-01 16:25:12 +0200

    Code format

--- src/Crocoder/Ex11.php
+++ src/Crocoder/Ex11.php
@@ -6,73 +6,73 @@ use Telema\Math;
 
 class Ex11
 {
-	public const EMPLOYEES = [
-		[
-			'name' => 'Jhon',
-			'salary' => 50000,
-			'department' => 'IT'
-		],
-		[
-			'name' => 'Jane',
-			'salary' => 60000,
-			'department' => 'HR'
-		],
-		[
-			'name' => 'Bob',
-			'salary' => 55000,
-			'department' => 'IT'
-		],
-		[
-			'name' => 'Sophie',
-			'salary' => 75000,
-			'department' => 'HR'
-		],
-		[
-			'name' => 'Mike',
-			'salary' => 65000,
-			'department' => 'IT'
-		],
-		[
-			'name' => 'Emily',
-			'salary' => 80000,
-			'department' => 'HR'
-		],
-		[
-			'name' => 'David',
-			'salary' => 70000,
-			'department' => 'IT'
-		]
-	];
+    public const EMPLOYEES = [
+        [
+            'name' => 'Jhon',
+            'salary' => 50000,
+            'department' => 'IT'
+        ],
+        [
+            'name' => 'Jane',
+            'salary' => 60000,
+            'department' => 'HR'
+        ],
+        [
+            'name' => 'Bob',
+            'salary' => 55000,
+            'department' => 'IT'
+        ],
+        [
+            'name' => 'Sophie',
+            'salary' => 75000,
+            'department' => 'HR'
+        ],
+        [
+            'name' => 'Mike',
+            'salary' => 65000,
+            'department' => 'IT'
+        ],
+        [
+            'name' => 'Emily',
+            'salary' => 80000,
+            'department' => 'HR'
+        ],
+        [
+            'name' => 'David',
+            'salary' => 70000,
+            'department' => 'IT'
+        ]
+    ];
 
-	public const HIGH_SALARY = 65000;
+    public const HIGH_SALARY = 65000;
 
-	protected function averageSalary($department)
-	{
-		$sum = array_sum(array_column($department, 'salary'));
-		$count = count($department);
-		return Math::avg($sum, $count);
-	}
+    protected function averageSalary($department)
+    {
+        $sum = array_sum(array_column($department, 'salary'));
+        $count = count($department);
+        return Math::avg($sum, $count);
+    }
 
-	public function __invoke()
-	{
-		$departmens = [];
-		$highSalaryDepartmens = [];
+    public function __invoke()
+    {
+        $departmens = [];
+        $highSalaryDepartmens = [];
 
-		foreach (self::EMPLOYEES as $employee) {
-			$departmens[$employee['department']][] = $employee;
-		}
+        foreach (self::EMPLOYEES as $employee) {
+            $departmens[$employee['department']][] = $employee;
+        }
 
-		foreach ($departmens as $departmentName => $department) {
-			$avgSalary = $this->averageSalary($department);
+        foreach ($departmens as $departmentName => $department) {
+            $avgSalary = $this->averageSalary($department);
 
-			if ($avgSalary > self::HIGH_SALARY) {
-				$highSalaryDepartmens[] = [
-					'department' => $departmentName,
-					'average' => $this->averageSalary($department)
-				];
-			}
-		}
+            if ($avgSalary > self::HIGH_SALARY) {
+                $highSalaryDepartmens[] = [
+                    'department' => $departmentName,
+                    'average' => $this->averageSalary($department)
+                ];
+            }
+        }
 
-		return $highSalaryDepartmens;
-	}
+        return $highSalaryDepartmens;
+    }
 }
--- src/Crocoder/Fp/Ex11.php
+++ src/Crocoder/Fp/Ex11.php
@@ -7,38 +7,36 @@ use Telema\Crocoder\Ex11 as BaseEx11;
 
 class Ex11 extends BaseEx11
 {
-	private function groupByCategory()
-	{
-		$fn = function ($departmens, $employee)
-		{	
-			$departmens[$employee['department']][] = $employee;
-			return $departmens;
-		};
+    private function groupByCategory()
+    {
+        $fn = function ($departmens, $employee) {
+            $departmens[$employee['department']][] = $employee;
+            return $departmens;
+        };
 
-		return array_reduce(self::EMPLOYEES, $fn, []);
-	}
+        return array_reduce(self::EMPLOYEES, $fn, []);
+    }
 
-	private function filterHighSalaryDepartmens($departmens)
-	{
-		$fn = function ($accum, $department)
-		{
-			$avgSalary = $this->averageSalary($department);
+    private function filterHighSalaryDepartmens($departmens)
+    {
+        $fn = function ($accum, $department) {
+            $avgSalary = $this->averageSalary($department);
 
-			if ($avgSalary > self::HIGH_SALARY) {
-				$accum[] = [
-					'department' => $department[0]['department'],
-					'average' => $this->averageSalary($department)
-				];
-			}
-			return $accum;
-		};
+            if ($avgSalary > self::HIGH_SALARY) {
+                $accum[] = [
+                    'department' => $department[0]['department'],
+                    'average' => $this->averageSalary($department)
+                ];
+            }
+            return $accum;
+        };
 
-		return array_reduce($departmens, $fn, []);
-	}
+        return array_reduce($departmens, $fn, []);
+    }
 
-	public function __invoke()
-	{	
-		$departmens = $this->groupByCategory();
-		return $this->filterHighSalaryDepartmens($departmens);
-	}
+    public function __invoke()
+    {
+        $departmens = $this->groupByCategory();
+        return $this->filterHighSalaryDepartmens($departmens);
+    }
 }
