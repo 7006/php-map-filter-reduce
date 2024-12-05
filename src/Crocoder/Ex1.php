23835231aa22788717e0bf80ebfa77bf473718df
oromadin <osromadin@gmail.com>
2024-12-05 16:21:16 +0200

    add new trait to Ex1

--- src/Crocoder/Ex1.php
+++ src/Crocoder/Ex1.php
@@ -2,14 +2,23 @@
 
 namespace Telema\Crocoder;
 
+trait Reader
+{
+    public static function getItems()
+    {
+        return [1, 2, 3, 4, 5];
+    }
+}
+
 class Ex1
 {
-    public const ITEMS = [1, 2, 3, 4, 5];
+    use Reader;
 
     public function __invoke()
     {
         $result = [];
-        foreach (self::ITEMS as $item) {
+        $items = self::getItems();
+        foreach ($items as $item) {
             $result[] = $item ** 2;
         }
         return $result;
