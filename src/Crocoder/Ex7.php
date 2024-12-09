a6600bc5a05369efe40ca7fd90defda829a4f9cb
oromadin <osromadin@gmail.com>
2024-12-09 20:41:27 +0200

    use callback and csv reader ex7

--- src/Crocoder/Ex7.php
+++ src/Crocoder/Ex7.php
@@ -4,15 +4,29 @@ namespace Telema\Crocoder;
 
 class Ex7
 {
-    public const NUMBER = 6;
+    use CsvReader;
+
+    public const FILE_PATH = __DIR__ . '/../../data/crocoder/ex07.csv';
 
     public function __invoke()
     {
+        $numbers = $this->readCsv(self::FILE_PATH, fn ($item) => $item['number']);
+
+        $factorials = [];
+        foreach ($numbers as $currentNumber) {
+            $factorials[] = $this->factorial($currentNumber);
+        }
+        return $factorials;
+    }
+
+    public function factorial($currentNumber)
+    {
         $factorial = 1;
 
-        foreach (range(1, self::NUMBER) as $number) {
+        foreach (range(1, $currentNumber) as $number) {
             $factorial *= $number;
         }
+
         return $factorial;
     }
 }
