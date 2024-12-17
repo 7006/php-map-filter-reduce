e8feef03f6d510348fa0d3254c2147d38669cd2e
oromadin <osromadin@gmail.com>
2024-12-17 14:14:37 +0200

    use csv reader ex2

--- src/Crocoder/Fp/Ex2.php
+++ src/Crocoder/Fp/Ex2.php
@@ -7,7 +7,8 @@ use Telema\Crocoder\Ex2 as BaseEx2;
 class Ex2 extends BaseEx2
 {
     public function __invoke()
-    {
-        return array_reduce(self::ITEMS, fn ($sum, $n) => $n > 0 ? $sum + $n : $sum, 0);
+    {   
+        $items = $this->readCsv(self::FILE_PATH, fn ($item) => $item['number']);
+        return array_reduce($items, fn ($sum, $n) => $n > 0 ? $sum + $n : $sum, 0);
     }
 }
