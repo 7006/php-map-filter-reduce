ecccf635dfd78c62e05ac58e0b9c71c498d17c19
oromadin <osromadin@gmail.com>
2024-12-17 14:08:00 +0200

    protected const to use extend

--- src/Crocoder/Ex1.php
+++ src/Crocoder/Ex1.php
@@ -8,7 +8,7 @@ class Ex1
 {
     use CsvReader;
 
-    private const FILE_PATH = __DIR__ . '/../../data/crocoder/ex01.csv';
+    protected const FILE_PATH = __DIR__ . '/../../data/crocoder/ex01.csv';
 
     public function __invoke()
     {
