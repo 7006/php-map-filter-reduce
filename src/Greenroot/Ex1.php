a664e09996ae81f8f12e93a112cc4df24f757aa5
oromadin <osromadin@gmail.com>
2024-12-18 19:52:53 +0200

    use json reader

--- src/Greenroot/Ex1.php
+++ src/Greenroot/Ex1.php
@@ -3,14 +3,25 @@
 namespace Telema\Greenroot;
 
 use Telema\Customer;
+use Telema\traits\JsonReader;
 
 class Ex1
 {
+    use JsonReader;
+
+    public const FILE_PATH = __DIR__ . '/../../data/greenroot/customers.json';
+
+    public function readCustomers()
+    {
+        $customers = $this->readJson(self::FILE_PATH);
+        return array_map(fn ($customer) => new Customer($customer), $customers);
+    }
+
     public function __invoke()
     {
         $seniorCitizens = [];
 
-        foreach (Customer::readCustomers() as $customer) {
+        foreach ($this->readCustomers() as $customer) {
             if ($customer->gtAge(60)) {
                 $seniorCitizens[] = $customer;
             }
