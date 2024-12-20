5d541bc1a6ba8f119f754845f152a715f3ed5b97
oromadin <osromadin@gmail.com>
2024-12-20 10:53:01 +0200

    use json reader fp/ex3

--- src/Greenroot/Fp/Ex3.php
+++ src/Greenroot/Fp/Ex3.php
@@ -4,12 +4,15 @@ namespace Telema\Greenroot\Fp;
 
 use Telema\Customer;
 use Telema\Math;
+use Telema\Greenroot\Traits\Customers;
 
 class Ex3
 {
+    use Customers;
+
     public function __invoke()
     {
-
+        
         $fn = function ($accum, $c) {
             [$age, $count] = $accum;
 
@@ -21,7 +24,7 @@ class Ex3
             return [$age, $count];
         };
 
-        [$age, $count] = array_reduce(Customer::readCustomers(), $fn, [0, 0]);
+        [$age, $count] = array_reduce($this->readCustomers(), $fn, [0, 0]);
 
         return Math::avg($age, $count);
     }
