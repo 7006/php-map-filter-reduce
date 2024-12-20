7695ce921ad03a8d22bd377312c33efcd57641d1
oromadin <osromadin@gmail.com>
2024-12-20 10:55:25 +0200

    use json reader fp/ex5

--- src/Greenroot/Fp/Ex5.php
+++ src/Greenroot/Fp/Ex5.php
@@ -3,12 +3,15 @@
 namespace Telema\Greenroot\Fp;
 
 use Telema\Customer;
+use Telema\Greenroot\Traits\Customers;
 
 class Ex5
-{
+{   
+    use Customers;
+
     public function __invoke()
     {
-        $customers = Customer::readCustomers();
+        $customers = $this->readCustomers();
         [$youngest] = Customer::sortByAge($customers);
 
         return $youngest;
