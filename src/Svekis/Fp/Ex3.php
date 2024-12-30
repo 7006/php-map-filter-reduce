9aff5924d3b9a2b01ac092818612b5da3b04a67b
oromadin <osromadin@gmail.com>
2024-12-30 16:07:58 +0200

    use csv reader svekis/fp/ex3

--- src/Svekis/Fp/Ex3.php
+++ src/Svekis/Fp/Ex3.php
@@ -7,7 +7,8 @@ use Telema\Svekis\Ex3 as BaseEx3;
 class Ex3 extends BaseEx3
 {
     public function __invoke()
-    {
-        return array_map(fn ($st) => $st['name'], self::ITEMS);
+    {   
+        $items = $this->readCsv(self::FILE_PATH, fn ($item) => $item);
+        return array_map(fn ($st) => $st['name'], $items);
     }
 }
