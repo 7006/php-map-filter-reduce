8f73c6ddbfbc2fdd642c43f3649a9f4b121d69a5
oromadin <osromadin@gmail.com>
2024-12-30 16:30:03 +0200

    use csv reader svekis/fp/ex4

--- src/Svekis/Fp/Ex4.php
+++ src/Svekis/Fp/Ex4.php
@@ -7,7 +7,8 @@ use Telema\Svekis\Ex4 as BaseEx4;
 class Ex4 extends BaseEx4
 {
     public function __invoke()
-    {
-        return array_map(self::celsiusToFahrenheit(...), self::ITEMS);
+    {   
+        $items = $this->readCsv(self::FILE_PATH, fn ($item) => $item['temp']);
+        return array_map(self::celsiusToFahrenheit(...), $items);
     }
 }
